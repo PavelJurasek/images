84ed2b8352984ebf5e0c12968e856356833b5a8b
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2020-01-04 16:54:53 +0100

    ImagesExtension: fix deprecated error

--- src/Brabijan/Images/DI/ImagesExtension.php
+++ src/Brabijan/Images/DI/ImagesExtension.php
@@ -39,7 +39,7 @@ class ImagesExtension extends Nette\DI\CompilerExtension
 		}
 
 		$builder->addDefinition($this->prefix('imagePipe'))->setClass('Brabijan\Images\ImagePipe', array($config['assetsDir'],
-			$this->containerBuilder->parameters['wwwDir']))
+			$this->getContainerBuilder()->parameters['wwwDir']))
 			->addSetup('setAssetsDir', array($config['assetsDir']));
 		$builder->addDefinition($this->prefix('imageStorage'))->setClass('Brabijan\Images\ImageStorage', array($config['assetsDir']));
 		$builder->addDefinition($this->prefix('fileBrowser'))->setClass('Brabijan\Images\FileBrowser');
